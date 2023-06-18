f7151a433fcbb27ffccb576e7597b17050000661
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-06-18 05:29:51 -0500

    Split NavLink generation into separate component

---
+++ src/react/components/LinkList.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import uniqid from 'uniqid';
+
+const LinkList = () => {
+    const linkNames = ['Home', "Men's Clothing", "Women's Clothing", 'Jewelery', 'Electronics'];
+    let navLinks = [];
+
+    function createNavLink(name) {
+        let navQuery;
+        switch (name) {
+            case 'Home':
+                navQuery = '';
+                break;
+            case "Men's Clothing":
+                navQuery = 'mens';
+                break;
+            case "Women's Clothing":
+                navQuery = 'womens';
+                break;
+            default:
+                navQuery = name.toLowerCase();
+        }
+        return (
+            <li key={uniqid()}>
+                <NavLink to={navQuery}>{name}</NavLink>
+            </li>
+        );
+    }
+
+    linkNames.forEach(item => {
+        navLinks.push(createNavLink(item));
+    });
+
+    return (
+        <ul>
+            {navLinks}
+        </ul>
+    )
+}
+
+export default LinkList;
--- src/react/components/Navigation.js
+++ src/react/components/Navigation.js
@@ -1,41 +1,18 @@
 import React from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-import uniqid from 'uniqid';
 
+import LinkList from './LinkList.js';
 import CartSummary from './CartSummary';
 
 const Navigation = () => {
     const navigate = useNavigate();
-
-    const linkNames = ['Home', "Men's Clothing", "Women's Clothing", 'Jewelery', 'Electronics'];
-    let navLinks = [];
-    for (let i = 0; i < linkNames.length; i++) {
-        let navText = linkNames[i];
-        let navQuery;
-        switch (navText) {
-            case 'Home':
-                navQuery = '';
-                break;
-            case "Men's Clothing":
-                navQuery = 'mens';
-                break;
-            case "Women's Clothing":
-                navQuery = 'womens';
-                break;
-            default:
-                navQuery = navText.toLowerCase();
-        }
-        navLinks.push(<li key={uniqid()}><NavLink to={navQuery}>{navText}</NavLink></li>);
-    }
 
     let back = '<<';
     let forw = '>>';
 
     return (
         <nav>
-            <ul>
-                {navLinks}
-            </ul>
+            <LinkList />
             <NavLink id='back' onClick={() => navigate(-1)}>{back}</NavLink>
             <NavLink id='forward' onClick={() => navigate(1)}>{forw}</NavLink>
             <CartSummary />
