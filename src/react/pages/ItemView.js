29fd99e39d24136bc6f59e7bfc949a2dea328ef1
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-06-23 07:09:05 -0500

    ItemView checks if item exists in cart, renders item action accordingly

--- src/react/pages/ItemView.js
+++ src/react/pages/ItemView.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import Counter from '../components/Counter';
 import AddButton from '../components/AddButton';
 import RemoveButton from '../components/RemoveButton';
 
-const ItemView = () => {
+const ItemView = (props) => {
+    const [ query, setQuery ] = useState(null);
     const { category, id } = useParams(); // NOTE: used for API call
-    const data = { // TEST: make state, replace with API fetch or state query
-        id: 1,
-        title: 'item 1',
+    const { getItem, view, addItem, removeItem, updateItem } = props;
+    let data = {
+        id: null,
+        title: null,
+        description: null,
+        price: null,
+        count: null,
+        image: null,
+    };
+    const mockFetch = { // TEST: replace with API fetch
+        id: 2,
+        title: 'item 2',
         description: 'description',
-        price: 100,
-        count: 1,
+        price: 200,
+        count: 0,
         image: 'imageURL'
     };
     let counter;
     let button;
 
-    if (data.count === 0) {
-        button = <AddButton />;
-    } else {
+    data = getItem(view);
+    if (data) {
         counter = <Counter />;
         button = <RemoveButton />;
+    } else {
+        data = mockFetch;
+        button = <AddButton />;
     }
 
     return (
@@ -38,6 +51,13 @@ const ItemView = () => {
             </section>
         </>
     )
+};
+ItemView.propTypes = {
+    getItem: PropTypes.func,
+    view: PropTypes.number,
+    addItem: PropTypes.func,
+    removeItem: PropTypes.func,
+    updateItem: PropTypes.func,
 }
 
 export default ItemView;
--- src/react/pages/Main.js
+++ src/react/pages/Main.js
@@ -8,12 +8,21 @@ import ItemView from './ItemView';
 import CartView from './CartView';
 
 const App = () => {
-    const [ cart, setCart ] = useState([]);
+    const [ cart, setCart ] = useState([
+        {
+            id: 1,
+            title: 'item 1',
+            description: 'description',
+            price: 100,
+            count: 1,
+            image: 'imageURL'
+        }
+    ]);
     const [ view, setView ] = useState(null);
 
     // state setters
     function updateView(value) {
-        setView(value);
+        setView(parseInt(value));
     }
     // state getters
     function getCount() {
@@ -21,16 +30,19 @@ const App = () => {
         cart.forEach(item => sum += item.count);
         return sum;
     }
+    function getItem(query) {
+        return cart.find(item => item.id === query);
+    }
     // state queriers
 
     return (
         <>
-            <Navigation count={getCount()}/>
+            <Navigation count={getCount()} />
             <section id='page'>
                 <Routes>
                     <Route path='/' element={<LandingView />} />
-                    <Route path='/:category' element={<CategoryView updateView={updateView}/>} />
-                    <Route path='/:category/:id' element={<ItemView />} />
+                    <Route path='/:category' element={<CategoryView updateView={updateView} />} />
+                    <Route path='/:category/:id' element={<ItemView getItem={getItem} view={view} />} />
                     <Route path='/cart' element={<CartView />} />
                 </Routes>
             </section>
