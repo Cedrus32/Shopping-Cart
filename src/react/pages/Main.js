6cb2a24ebe365050a6b65d1267acd337e0bfb89f
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-06-23 07:38:40 -0500

    Correctly set count for new items when adding to state

--- src/react/pages/Main.js
+++ src/react/pages/Main.js
@@ -9,14 +9,14 @@ import CartView from './CartView';
 
 const App = () => {
     const [ cart, setCart ] = useState([
-        {
-            id: 1,
-            title: 'item 1',
-            description: 'description',
-            price: 100,
-            count: 1,
-            image: 'imageURL'
-        }
+        // {
+        //     id: 1,
+        //     title: 'item 1',
+        //     description: 'description',
+        //     price: 100,
+        //     count: 1,
+        //     image: 'imageURL'
+        // }
     ]);
     const [ view, setView ] = useState(null);
 
@@ -24,26 +24,38 @@ const App = () => {
     function updateView(value) {
         setView(parseInt(value));
     }
+    function addItem(data) {
+        setCart([
+            ...cart,
+            {
+                id: data.id,
+                title: data.title,
+                description: data.description,
+                price: data.price,
+                count: data.count + 1,
+                image: data.image,
+            }
+        ]);
+    }
     // state getters
-    function getCount() {
+    function getCount(data) {
+        console.log(data)
         let sum = 0;
-        cart.forEach(item => sum += item.count);
+        data.forEach(item => sum += item.count);
         return sum;
     }
     function getItem(query) {
-        console.log(query);
         return cart.find(item => item.id === query);
     }
-    // state queriers
 
     return (
         <>
-            <Navigation count={getCount()} />
+            <Navigation count={getCount(cart)} />
             <section id='page'>
                 <Routes>
                     <Route path='/' element={<LandingView />} />
                     <Route path='/:category' element={<CategoryView updateView={updateView} />} />
-                    <Route path='/:category/:id' element={<ItemView cartItem={getItem(view)} />} />
+                    <Route path='/:category/:id' element={<ItemView cartItem={getItem(view)} addItem={addItem} />} />
                     <Route path='/cart' element={<CartView />} />
                 </Routes>
             </section>
