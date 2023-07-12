c4eecac31f75e5c7458ec9c23d3034b0e1a6417f
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-07-12 06:34:38 -0500

    Remove logic to get categories -- this was moved into useStore hook

--- src/hooks/useGetCat.js
+++ src/hooks/useGetCat.js
@@ -1,23 +1,11 @@
-import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { StoreContext } from '../contexts/StoreContext';
+import useStore from '../hooks/useStore';
 
 const useGetCat = () => {
     const { category } = useParams();
-    const { store } = useContext(StoreContext);
-    let data;
-
-    if (category === 'all') {
-        data = store;
-    } else {
-        let itemData = [];
-        let items = store.filter(item => item.category === category);
-        items.forEach(item => {
-            itemData.push({id: item.id, title: item.title, price: item.price, image: item.image});
-        });
-        data = itemData;
-    }
+    const { getCat } = useStore();
+    let data = getCat(category);
 
     return {
         data,
